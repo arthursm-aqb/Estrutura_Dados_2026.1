090142c11d6d7e53edfaea26069be79a8dce00f3
Arthur Mariz <arthur.mariz_silva@hotmail.com>
2026-04-28 23:58:47 -0300

    vetor circular tad update

--- TAD_vetor/vetorCircular.java
+++ TAD_vetor/vetorCircular.java
@@ -2,7 +2,7 @@ package TAD_vetor;
 
 public class vetorCircular implements vetor {
     
-    private int[] circular;
+    private Object[] circular;
     private int tam, capacidade, inicio;
 
     public vetorCircular(int capacidade){
@@ -10,16 +10,39 @@ public class vetorCircular implements vetor {
         this.capacidade = capacidade;
         this.tam = 0;
         this.inicio = 0;
-        this.circular = new int[capacidade];
+        this.circular = new Object[capacidade];
     }
 
     public Object elemAtRank(int rank){
         if(rank < 0 || rank >= this.tam) throw new IndexOutOfBoundsException("Rank inválido!");
-        return circular[(this.inicio + rank) % this.capacidade];
+        return this.circular[(this.inicio + rank) % this.capacidade];
     }
 
-    public Object replaceAtRank(int rank, Object o);
-    public void insertAtRank(int rank, Object o);
+    public Object replaceAtRank(int rank, Object o){
+        if(rank < 0 || rank >= this.tam) throw new IndexOutOfBoundsException("Rank inválido!");
+
+        Object retorno = this.elemAtRank(rank);
+
+        this.circular[(this.inicio + rank) % this.capacidade] = o;
+
+        return retorno;
+    }
+
+    public void insertAtRank(int rank, Object o){
+        if(rank < 0 || rank >= this.tam) throw new IndexOutOfBoundsException("Rank inválido!");
+        if(this.size()==this.capacidade) throw new RuntimeException("Vetor cheio");
+
+
+        if(rank<(this.size()/2)){
+            this.inicio = (this.inicio - 1 + this.capacidade) % this.capacidade;
+            for(int i = 0; i<rank; i++) circular[(this.inicio + i) % this.capacidade] = circular[(this.inicio + i + 1) % this.capacidade];
+        } else{
+            for(int i = this.size(); i>rank; i--) this.circular[(this.inicio+i) % this.capacidade] = this.circular[(this.inicio + i - 1) % this.capacidade];
+        }
+
+        circular[(this.inicio + rank) % this.capacidade] = o;
+        this.tam++;
+    }
     public Object removeAtRank(int rank);
 
     public int size(){
